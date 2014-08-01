6f87db078a80291ef9f14221aa7b898d922e6ff7
netcom-apple <netcom-apple@netcom-appledeiMac.local>
2014-08-01 09:43:06 +0800

    Initial Commit

---
+++ swiftlearn/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftlearn
+//
+//  Created by netcom-apple on 14-8-1.
+//  Copyright (c) 2014年 netcom-apple. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
